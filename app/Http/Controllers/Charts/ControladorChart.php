780554b08c16d3db72165a359b01953c77e9b4d9
marcoleon <paramore_malj@outlook.com>
2016-11-06 16:39:25 -0600

    solo los administradores pueden entrar al dashboard

--- app/Http/Controllers/Charts/ControladorChart.php
+++ app/Http/Controllers/Charts/ControladorChart.php
@@ -6,11 +6,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use pinkfeelin\Http\Requests;
 use pinkfeelin\Http\Controllers\Controller;
+use Auth;
 
 class ControladorChart extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware("auth");
+  }
     public function index()
     {
+      if (Auth::user()->tipo==1) {
+
+
       $clientes =\pinkfeelin\Models\User\User::select(DB::raw('count(id) as cantidad'), 'sexo')->where('tipo','=','0')->groupBy('sexo')->get();
       $nuevos = \pinkfeelin\Models\User\User::select(DB::raw('count(id) as conteo, month(created_at) as mes'))->where(DB::raw('month(created_at)'),'=', DB::raw('month(sysdate())'))->get();
       $ventames = \pinkfeelin\Models\Buy\Buy::select(DB::raw('sum(replace(importe,\',\',\'\')) as total'))->where(DB::raw('month(created_at)'), '=',DB::raw('month(sysdate())'))->get();
@@ -20,5 +28,9 @@ class ControladorChart extends Controller
       $tarjetas=\pinkfeelin\Models\User\Payment::select(DB::raw('count(id) as ventas, if(numero not like "",\'tarjeta\',\'otro\') as tipodepago, tipo '))->groupBy('tipodepago','tipo')->get();
       $compras= \pinkfeelin\Models\User\User::select(DB::raw('name, lastname,year(sysdate())-substring_index(fecha_nac,\',\',-1) as edad, sexo, count(buys.id) as compras, sum(replace(importe,\',\',\'\')) as total_compras'))->join('buys','users.id','=','buys.id_usuario')->groupBy('users.id')->get();
       return view('chart')->with('clientes',$clientes)->with('nuevos', $nuevos)->with('ventames', $ventames)->with('masvendidos', $masvendidos)->with('envios', $envios)->with('pagos', $pagos)->with('tarjetas', $tarjetas)->with('compras', $compras);
+    }else {
+
+      return view('index');
+    }
     }
 }
